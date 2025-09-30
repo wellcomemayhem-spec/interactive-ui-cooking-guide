aecd5a1cefc30c17531769f0c6f0311b8a048a7c
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-09-30 22:42:31 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Interactive UI Cooking Guide</h1>
+        <p>Discover and cook delicious recipes!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
